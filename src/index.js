ee16c81374958ae3754b5b53c5066c3b0cd1bfa9
Shashank <shiv04313@gmail.com>
2024-10-08 19:10:16 +0530

    utilities added

--- src/index.js
+++ src/index.js
@@ -6,7 +6,14 @@ dotenv.config({
     path:'./env'
 })
 connectDB()
-
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running on port ${process.env.PORT || 8000}`)
+    });
+})
+.catch((err)=>{
+    console.log("Error: ",err);
+})
 
 
 
