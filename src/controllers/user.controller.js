80d6df998c5956e3908048d2cd897ec2a1b4ba91
Shashank <shiv04313@gmail.com>
2024-12-29 00:09:49 +0530

    fix: correct logic for refresh token validation in logoutUser

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -140,7 +140,7 @@ const logoutUser = asyncHandler(async(req,res)=>{
 
 const refreshAccessToken = asyncHandler(async(req,res)=>{
   const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken;
-  if(incomingRefreshToken){
+  if(!incomingRefreshToken){
     throw new ApiError(401,"Unauthorized Request");
   }
   const decodedToken = jwt.verify(incomingRefreshToken, process.env.ACCESS_TOKEN_SECRET);
